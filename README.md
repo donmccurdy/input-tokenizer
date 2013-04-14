9fb8c5a0b495cb1a28e43ef2c81e89447aa08f45
Don McCurdy <don.r.mccurdy@gmail.com>
2013-04-13 19:40:17 -0700

    Replaced essential code. Deleting old files.

--- README.md
+++ README.md
@@ -15,35 +15,72 @@ For a live demo, check here: http://www.donmccurdy.net/sandbox/tag_editor/demo/
 
 To get started, here are the steps:
 
-* First, you'll need jQuery and (very optionally) this one font that I like. Put these lines in your HTML header.
+* First, you'll need jQuery and my plugin. jQuery should be included first, and the path to the plugin will depend on where you put it. For example, put this in the <head></head> tags of your HTML:
 
 ```html
-    <link href='http://fonts.googleapis.com/css?family=Quicksand:300,400' rel='stylesheet' type='text/css'>
-    <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js" ></script>
+    <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js" ></script>
+		<script type="text/javascript" src="tokenizer.js"></script>
 ```
 
-* Next, download the .CSS and .JS files from GitHub, and include them as well:
+* Next, copy my sample CSS or tweak it to your needs, then include it as well:
 
 ```html
     <link rel="stylesheet" type="text/css" href="input_style.css" />
-    <script type="text/javascript" src="scripts.js"></script>
 ```
 
-* Finally, add the input form to your page. Like this:
+* Finally, call the plugin on your input(s). If you've added the ".myTokenInput" class to yours, you might do something like this:
 
 ```html
-    <div class="inputWrapper">
-        <span class="tokenListLabel">Tags:</span>
-        <div class="tokenList"></div>
-        <input type="text" class="tokenInput" />
-    </div>
+    <script type="text/javascript">
+			var tokenInput = $('input.myTokenInput').tokenizer({ /* options */ });
+		</script>
 ```
 
 * You're done! Mess around with the CSS if you want to restyle things a bit.
 
+* If you need to do real work with this plugin, you'll probably want to know the methods and options. Here they are:
+
+```
+	// Initialize with default options
+	$(input).tokenizer({});
+
+	// Default options:
+	options = {
+				showX: true, // show button to delete a tag
+				xContent: '&times;', // content of the delete button
+				namespace: 'tknz', // used as CSS prefix and event namespace
+				label: 'Tags:', // Label at top of input
+				separators: [',', ' ', '.'], // Trigger characters to separate tokens. Use [',', '.'] to allow multiple words per tag.
+				callback: null // function that will be called when the token list changes. The original <input> is passed as a parameter.
+	}
+
+	// Initialize:
+	$(input).tokenizer(options);
+	
+	// Get list of tokens
+	var list = $(input).tokenizer('get'); 	// ['unsought','stuffed','dogs']
+
+	// Manually add a token
+	$(input).tokenizer('push', 'YOLO'); // adds 'YOLO' to tokens.
+
+	// Get the most recent token
+	var lastToken = $(input).tokenizer('pop'); //Returns last token in list.
+	
+	// Clear the input
+	$(input).tokenizer('empty'); // Token list is now empty.
+
+	// Un-tokenize the input (returns everything to pre-plugin state)
+	$(input).tokenizer('destroy'); // Just a normal input now.
+
+	// Manually trigger the callback function
+	$(input).tokenizer('callback'); // triggers provided callback, if any.
+
+
+```
+
 - - -
 
-If this is a bit more basic than you need, there are some great plugins others have put together available. Try these, for starters:
+If this isn't what you need, there are other great ones out there. Try these, for starters:
 
 * http://aehlke.github.com/tag-it/ (recommended)
 * http://xoxco.com/projects/code/tagsinput/
@@ -52,4 +89,4 @@ If this is a bit more basic than you need, there are some great plugins others h
 
 - - -
 
-Created by Don McCurdy. Do feel free to use this for whatever you want, although I'd appreciate it if you'd either cite me or let me know if you find a cool use for my code. Thanks! 
+Created by Don McCurdy. Do feel free to use this for whatever you want, although I'd appreciate it if you'd cite me in the comments. Or let me know if you find a cool use for my code. Thanks! 
--- Tokenizer.js
+++ Tokenizer.js
@@ -5,116 +5,160 @@
 
 (function ($) {
 
-  	var Tokenizer = function (argElement, argOpts) {
-  		// PRIVATE VARS
-  		var
-  			input, 
-  			list,
-  			wrap,
-  			options = $.extend({
-  				foo: 'bar',
-  				classSpace: 'tknz',
-  				eventSpace: 'tknz',
-  				label: 'Tags:',
-  				separators: [13,188,32]
-  			}, argOpts);
+	var Tokenizer = function (argElement, argOpts) {
+		// PRIVATE VARS
+		var
+			input, 
+			list,
+			wrap,
+			options = $.extend({
+				showX: true,
+				xContent: '&times;',
+				namespace: 'tknz',
+				label: 'Tags:',
+				separators: [',', ' ', '.'],
+				callback: null
+			}, argOpts);
 
 		// PRIVATE METHODS
-  		var init, buildHTML, bindEvents, push, pop, get, empty, destroy;
+		var init, buildHTML, bindEvents, push, pop, get, empty, destroy, callback;
 
-  		init = function () {
-  			input = argElement;
-  			buildHTML();
-  			bindEvents();
-  		};
-  		buildHTML = function () {
-  			// classSpaceing!
-  			wrap = $('<div class="inputWrapper"></div>');
-  			list = $('<div class="tokenList"></div>');
-  			input.wrap(wrap);
-  			wrap
-  				.prepend(list)
-  				.prepend('<span class="tokenListLabel">'+options.label+'</span>');
-  		};
-  		bindEvents = function () {
-  			wrap.on('focus.'+options.eventSpace, 'input', function () {
-  				wrap.addClass('focusField');
-  			}).on('blur.'+options.eventSpace, 'input', function () {
-  				wrap.removeClass('focusField');
-  			}).on('keyup.'+options.eventSpace, 'input', function (event) {
-  				if (options.separators.indexOf(event.which) > -1 && input.val()) {
-					push(input.val());
-					input.val('');
-  				} else if (event.which === 8 && !input.val()) {
-  					pop();
-  				}
-  			}).on('click.'+options.eventSpace, function () {
-  				input.focus();
-  			});
-  		};
-  		push = function (value) {
-  			list.append('<div class="tokenLabel">'+value+'</div>');
-  			return input;
-  		};
-  		pop = function () {
-  			return list.children().last().remove().html();
-  		};
-  		empty = function () {
-  			list.empty();
-  			return input;
-  		};
-  		get = function () {
-  			return [1,3,4];
-  		};
-  		destroy = function () {
-  			wrap.off('.'+options.eventSpace);
+		init = function () {
+			input = argElement;
+			buildHTML();
+			bindEvents();
+		};
+		buildHTML = function () {
+			var ns = options.namespace;
+		  	list = $('<div class="'+ns+'-list"></div>');
+			wrap = input
+				.addClass(ns+'-input')
+			 	.wrap('<div class="'+ns+'-wrapper"></div>')
+			 	.parent()
+				.prepend(list)
+				.prepend('<span class="'+ns+'-wrapper-label">'+options.label+'</span>');
+		};
+		bindEvents = function () {
+			var ns = options.namespace;
+			wrap
+				.on('focus.'+ns, 'input', function () {  // On focus, stylize wrapper.
+					wrap.addClass(ns+'-focus');
+				}).on('blur.'+ns, 'input', function () { // On blur, un-stylize.
+					wrap.removeClass(ns+'-focus');
+					if (input.val()) { 
+						push(input.val()); 
+						input.val('');
+						callback();
+					}
+				}).on('keydown.'+ns, 'input', function () { // Backspace handler.
+			 		if (event.which === 8 && !input.val()) {
+						pop();
+						callback();
+					}
+		  		}).on('keypress.'+ns, 'input', function (event) { // Input listener to create tokens.
+			 		var val = input.val();
+					if (options.separators.indexOf(String.fromCharCode(event.which)) > -1 || event.which === 13) {
+						event.preventDefault();
+						if (val) { 
+							push(val); 
+							callback();
+						}
+						input.val('');
+					}
+				}).on('click.'+ns, function () {	// On click, focus the input.
+					input.focus();
+				}).on('click.'+ns, '.'+ns+'-token-x', function () {
+					$(this).closest('.'+ns+'-token').remove();
+					callback();
+				});
+		};
 
-  			return input;
-  		};
-
-  		
-  		init (argElement);
-  		return {
-  			push: push,
-  			pop: pop,
-  			empty: empty,
-  			get: get,
-  			destroy: destroy
-  		};
-  	};
+		push = function (value) {
+		  	var
+		  		ns = options.namespace, 
+		  		token = '<div class="'+ns+'-token" data-token="'+value+'">'+
+						'<span class="'+ns+'-token-label">'+value.trim()+'</span>'+
+						'<span class="'+ns+'-token-x">'+options.xContent+'</span>'+
+			 		'</div>';
+			list.append(token);
+			return input;
+		};
+		pop = function () {
+			return list.children().last().detach().data('token') || null;
+		};
+		empty = function () {
+			list.empty();
+			return input;
+		};
+		get = function () {
+			var 
+				i, 
+				tokenList = [], 
+				tokens = list.children();
+			for (i = 0; i < tokens.length; i++) {
+				tokenList.push(tokens.eq(i).data('token').toString());
+			}
+			return tokenList;
+		};
+		destroy = function () {
+			wrap.after(input).remove();
+			return input.removeClass(options.namespace+'-input');
+		};
+		callback = function () {
+			(options.callback || $.noop)(input);
+			return input;
+		};
+		
+		init (argElement);
+		return {
+			push: push,
+			pop: pop,
+			empty: empty,
+			get: get,
+			destroy: destroy,
+			callback: callback
+		};
+	};
 
 
 	// PUBLIC METHODS
-  	var methods = {
-	   init: function( options ) { 
-	   	// check that this[0].nodeName === 'INPUT'
-	   	return this.data('tokenizer', Tokenizer(this, options));
-	   },
-	   push: function(value) {
-	   	return this.data('tokenizer').push(value);
-	   },
-	   pop: function() { 
-	    	return this.data('tokenizer').pop();
-	   },
-	   empty : function( content ) { 
-	    	return this.data('tokenizer').empty(); },
-	   get: function () {
-	   	return this.data('tokenizer').get();
-	   },
-	   destroy: function () {
-	   	return this.data('tokenizer').destroy();
-	   }
-  	};
+	var methods = {
+		init: function( options ) { 
+		if (this[0].nodeName !== 'INPUT') {
+		  $.error('Tokenizer must be set up with an <input type="text"> tag.');
+		  return this;
+		}
+			return this.data('tokenizer', Tokenizer(this, options));
+		},
+		push: function(value) {
+			return this.data('tokenizer').push(value);
+		},
+		pop: function() { 
+		 	return this.data('tokenizer').pop();
+		},
+		empty : function( content ) { 
+		 	return this.data('tokenizer').empty(); },
+		get: function () {
+			return this.data('tokenizer').get();
+		},
+		destroy: function () {
+			return this.data('tokenizer').destroy();
+		},
+		callback: function () {
+			return this.data('tokenizer').callback();
+		}
+	};
 
 
-  	// EXPORT PLUGIN
-  	$.fn.tokenizer = function( method ) {
-	   if ( methods[method] ) {
-	      return methods[ method ].apply( this, Array.prototype.slice.call( arguments, 1 ));
-	   } else if ( typeof method === 'object' || ! method ) {
-	      return methods.init.apply( this, arguments );
-	   } else {
-	      $.error( 'Method ' +  method + ' does not exist on jQuery.tokenizer' );
-	   }    
+	// EXPORT PLUGIN
+	$.fn.tokenizer = function( method ) {
+		if ( methods[method] ) {
+			if (!this.data('tokenizer')) { $.error('Cannot call method "'+method+'" - Tokenizer has not been initialized on this input.'); }
+			return methods[ method ].apply( this, Array.prototype.slice.call( arguments, 1 ));
+		} else if ( typeof method === 'object' || ! method ) {
+			return methods.init.apply( this, arguments );
+		} else {
+			$.error( 'Method ' +  method + ' does not exist on jQuery.tokenizer' );
+		}    
   };
 })(jQuery);
