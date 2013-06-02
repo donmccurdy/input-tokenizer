218ec1058120dbe8a51881dab20eae837cc75d5b
Don McCurdy <don.r.mccurdy@gmail.com>
2013-06-01 19:49:22 -0700

    Incr 4, autocomplete.

--- tokenizer.js
+++ tokenizer.js
@@ -4,29 +4,32 @@
 /*********** INITIALIZATION ************/
 
 (function ($) {
+	var tokenizer = 'tokenizer';
 
 	var Tokenizer = function (argElement, argOpts) {
 
 		// PRIVATE VARS
 		var
-			input, 
-			list,
-			wrap,
-			suggestions,
-			options = $.extend({
-				xContent: '&times;',
-				namespace: 'tknz',
-				label: 'Tags:',
-				separators: [',', ' ', '.'],
-				callback: null,
-				source: ['one','two','three','four','five'],
-				allowUnknownTags: false,
-				numToSuggest: 5
-			}, argOpts);
+		input, 
+		list,
+		wrap,
+		suggestions,
+		options = $.extend({
+			xContent: '&times;',
+			namespace: 'tknz',
+			label: 'Tags:',
+			placeholder: '',
+			separators: [',', ' ', '.'],
+			callback: null,
+			source: null,
+			allowUnknownTags: true,
+			numToSuggest: 5,
+			onclick: null
+		}, argOpts);
 
 		// PRIVATE METHODS
 		var init, buildHTML, bindEvents, push, pop, get, empty, 
-			destroy, callback, suggest, getMatch, tryPush;
+		destroy, callback, suggest, getMatch, tryPush, escapeRegExp;
 
 		init = function () {
 			input = argElement;
@@ -35,41 +38,42 @@
 		};
 		buildHTML = function () {
 			var ns = options.namespace;
-		  	list = $('<div class="'+ns+'-list"></div>');
-		  	suggestions = $('<div class="'+ns+'-suggest"><ul></ul></div>');
+			list = $('<div class="'+ns+'-list"></div>');
+			if (options.placeholder) { input.attr('placeholder', options.placeholder); }
+			suggestions = $('<div class="'+ns+'-suggest"><ul></ul></div>');
 			wrap = input
-				.addClass(ns+'-input')
-				.wrap('<span class="'+ns+'-input-wrapper"></span>')
-				.parent()
-			 	.wrap('<div class="'+ns+'-wrapper"></div>')
-			 	.parent()
-				.prepend(list)
-				.prepend('<span class="'+ns+'-wrapper-label">'+options.label+'</span>')
-				.find('.'+ns+'-input-wrapper').append(suggestions).end();
+			.addClass(ns+'-input')
+			.wrap('<span class="'+ns+'-input-wrapper"></span>')
+			.parent()
+			.wrap('<div class="'+ns+'-wrapper"></div>')
+			.parent()
+			.prepend(list)
+			.prepend('<span class="'+ns+'-wrapper-label">'+options.label+'</span>')
+			.find('.'+ns+'-input-wrapper').append(suggestions).end();
 		};
 		bindEvents = function () {
 			var ns = options.namespace;
 			wrap
-				.on('focus.'+ns, 'input', function () {  // On focus, stylize wrapper.
+				.on('focus', 'input', function () {  // On focus, stylize wrapper.
 					wrap.addClass(ns+'-focus');
-				}).on('blur.'+ns, 'input', function () { // On blur, un-stylize.
+				}).on('blur', 'input', function () { // On blur, un-stylize.
 					wrap.removeClass(ns+'-focus');
 					tryPush(input.val());
-				}).on('keydown.'+ns, 'input', function () { // Backspace handler.
-			 		if (event.which === 8 && !input.val()) {
+				}).on('keydown', 'input', function () { // Backspace handler.
+					if (event.which === 8 && !input.val()) {
 						pop();
 						callback();
 						return;
 					} 
 					var 
-						selectClass = ns+'-sel',
-						selected = suggestions.find('.'+selectClass);
-						
+					selectClass = ns+'-sel',
+					selected = suggestions.find('.'+selectClass);
+
 					if (event.which === 38) { // Up
 						event.preventDefault();
 						if (selected.length) {
 							selected.removeClass(selectClass)
-								.prev('li').add(selected.siblings().last()).eq(0).addClass(selectClass);
+							.prev('li').add(selected.siblings().last()).eq(0).addClass(selectClass);
 						} else {
 							suggestions.find('li').last().addClass(selectClass);
 						}
@@ -77,153 +81,164 @@
 						event.preventDefault();
 						if (selected.length) {
 							selected.removeClass(selectClass)
-								.next('li').addClass(selectClass);
+							.next('li').addClass(selectClass);
 						} else {
 							suggestions.find('li').first().addClass(selectClass);
 						}
 					}
-		  		}).on('keypress.'+ns, 'input', function (event) { // Input listener to create tokens.
-			 		var val = input.val();
-					if (options.separators.indexOf(String.fromCharCode(event.which)) > -1 || event.which === 13) {
-						event.preventDefault();
-						tryPush(val);
-					}
-				}).on('keyup.'+ns, 'input', function (event) {
-					if (event.which === 38 || event.which === 40) { return; }
+		  		}).on('keypress', 'input', function (event) { // Input listener to create tokens.
+		  			if (options.separators.indexOf(String.fromCharCode(event.which)) > -1 || event.which === 13) {
+		  				event.preventDefault();
+		  				tryPush(input.val());
+		  			}
+		  		}).on('keyup', 'input', function (event) {
+		  			if (event.which === 38 || event.which === 40) { return; }
 					if ($.isArray(options.source)) { // Autosuggest from list
 						suggest(input.val(), options.source);
 					} else if (options.source) { // Autosuggest from function
 						options.source(input.val(), suggest);
 					}
-				}).on('click.'+ns, function () {	// On click, focus the input.
+				}).on('click', function () {	// On click, focus the input.
 					input.focus();
-				}).on('click.'+ns, '.'+ns+'-token-x', function () {
+				}).on('click', '.'+ns+'-token-x', function (event) {
+					event.stopPropagation();
 					$(this).closest('.'+ns+'-token').remove();
 					callback();
-				}).on('click.'+ns, '.'+ns+'-suggest-li', function () {
+				}).on('click', '.'+ns+'-suggest-li', function () {
 					input.val('');
 					push($(this).text());
 					suggest('', []);
 					callback();
+				}).on('click', '.'+ns+'-token', function (event) {
+					if (options.onclick) { 
+						event.stopPropagation();
+						options.onclick($(this).children('.'+ns+'-token-label').text()); 
+					}
 				});
-		};
+			};
 
-		tryPush = function (value) {
-			var match = getMatch();
-			if (value && (options.allowUnknownTags || match)) { 
-				push(match || value); 
-				input.val('');
-				callback();
-			}
-			suggest('', []);
-		};
-		push = function (value) {
-		  	var
-		  		ns = options.namespace, 
-		  		token = '<div class="'+ns+'-token" data-token="'+value+'">'+
-						'<span class="'+ns+'-token-label">'+value.trim()+'</span>'+
-						'<span class="'+ns+'-token-x">'+options.xContent+'</span>'+
-			 		'</div>';
-			list.append(token);
-			return input;
-		};
-		pop = function () {
-			return list.children().last().detach().data('token') || null;
-		};
-		empty = function () {
-			list.empty();
-			return input;
-		};
-		get = function () {
-			var 
+			tryPush = function (value) {
+				var match = getMatch();
+				if (value && (options.allowUnknownTags || match)) { 
+					push(match || value); 
+					input.val('');
+					callback();
+				}
+				suggest('', []);
+			};
+			push = function (value) {
+				var
+				ns = options.namespace, 
+				pre = ns+'-token',
+				token = '<div class="'+pre+'" data-token="'+value+'">'+
+				'<span class="'+pre+'-label">'+value.trim()+'</span>'+
+				'<span class="'+pre+'-x">'+options.xContent+'</span>'+
+				'</div>';
+				list.append(token);
+				return input;
+			};
+			pop = function () {
+				return list.children().last().detach().data('token') || null;
+			};
+			empty = function () {
+				list.empty();
+				return input;
+			};
+			get = function () {
+				var 
 				i, 
 				tokenList = [], 
 				tokens = list.children();
-			for (i = 0; i < tokens.length; i++) {
-				tokenList.push(tokens.eq(i).data('token').toString());
-			}
-			return tokenList;
-		};
-		destroy = function () {
-			wrap.after(input).remove();
-			return input.removeClass(options.namespace+'-input');
-		};
-		callback = function () {
-			(options.callback || $.noop)(input);
-			return input;
-		};
-		suggest = function (word, words) {
-			var 
+				for (i = 0; i < tokens.length; i++) {
+					tokenList.push(tokens.eq(i).data('token').toString());
+				}
+				return tokenList;
+			};
+			destroy = function () {
+				wrap.after(input).remove();
+				if (options.placeholder) { input.attr('placeholder', ''); }
+				return input.removeClass(options.namespace+'-input');
+			};
+			callback = function () {
+				(options.callback || $.noop)(input);
+				return input;
+			};
+			suggest = function (word, words) {
+				var 
 				i, 
 				ns = options.namespace,
-				re1 = new RegExp(word, 'i'),
-				re2 = new RegExp('^'+word+'$', 'i'),
+				re1 = new RegExp(escapeRegExp(word), 'i'),
+				re2 = new RegExp('^'+escapeRegExp(word)+'$', 'i'),
 				limit = options.numToSuggest || 1000,
-				list = '';
-			for (i = 0; word && i < words.length && i < limit; i++) {
-				if (!words[i].match(re1)) { continue; }
-				list += '<li class="'+ns+'-suggest-li'+
-					(words[i].match(re2) ? ' '+ns+'-sel' : '')+'">'+words[i]+'</li>';
-			}
-			suggestions.children('ul')
-				.html(list)
+				list = [];
+				for (i = 0; word && i < words.length && list.length < limit; i++) {
+					if (!words[i].match(re1)) { continue; }
+					list.push('<li class="'+ns+'-suggest-li'+
+						(words[i].match(re2) ? ' '+ns+'-sel' : '')+'">'+words[i]+'</li>');
+				}
+				suggestions.children('ul')
+				.html(list.join(''))
 				.end()
-				[list ? 'addClass' : 'removeClass']('.'+ns+'-vis');
+				[list.length ? 'addClass' : 'removeClass']('.'+ns+'-vis');
+			};
+			getMatch = function () {
+				return suggestions.find('.'+options.namespace+'-sel').eq(0).text();
+			};
+			escapeRegExp = function (str) {
+				return str.replace(/[\-\[\]\/\{\}\(\)\*\+\?\.\\\^\$\|]/g, "\\$&");
+			};
+
+			init (argElement);
+			return {
+				push: push,
+				pop: pop,
+				empty: empty,
+				get: get,
+				destroy: destroy,
+				callback: callback
+			};
 		};
-		getMatch = function () {
-			return suggestions.find('.'+options.namespace+'-sel').eq(0).text();
-		};
-		
-		init (argElement);
-		return {
-			push: push,
-			pop: pop,
-			empty: empty,
-			get: get,
-			destroy: destroy,
-			callback: callback
-		};
-	};
 
 
 	// PUBLIC METHODS
 	var methods = {
 		init: function( options ) { 
-		if (this[0].nodeName !== 'INPUT') {
-		  $.error('Tokenizer must be set up with an <input type="text"> tag.');
-		  return this;
-		}
-			return this.data('tokenizer', Tokenizer(this, options));
+			if (this[0].nodeName !== 'INPUT') {
+				$.error('Tokenizer requires an <input type="text"> tag.');
+				return this;
+			}
+			return this.data(tokenizer, Tokenizer(this, options));
 		},
 		push: function(value) {
-			return this.data('tokenizer').push(value);
+			return this.data(tokenizer).push(value);
 		},
 		pop: function() { 
-		 	return this.data('tokenizer').pop();
+			return this.data(tokenizer).pop();
 		},
-		empty : function( content ) { 
-		 	return this.data('tokenizer').empty(); },
+		empty : function() { 
+			return this.data(tokenizer).empty(); 
+		},
 		get: function () {
-			return this.data('tokenizer').get();
+			return this.data(tokenizer).get();
 		},
 		destroy: function () {
-			return this.data('tokenizer').destroy();
+			return this.data(tokenizer).destroy();
 		},
 		callback: function () {
-			return this.data('tokenizer').callback();
+			return this.data(tokenizer).callback();
 		}
 	};
 
 
 	// EXPORT PLUGIN
-	$.fn.tokenizer = function( method ) {
+	$.fn[tokenizer] = function( method ) {
 		if ( methods[method] ) {
-			if (!this.data('tokenizer')) { $.error('Cannot call method "'+method+'" - Tokenizer has not been initialized on this input.'); }
+			if (!this.data(tokenizer)) { $.error('Cannot call "'+method+'" - Tokenizer not initialized.'); }
 			return methods[ method ].apply( this, Array.prototype.slice.call( arguments, 1 ));
 		} else if ( typeof method === 'object' || ! method ) {
 			return methods.init.apply( this, arguments );
 		} else {
-			$.error( 'Method ' +  method + ' does not exist on jQuery.tokenizer' );
+			$.error( 'Unknown tokenizer method ' +  method + '.' );
 		}    
-  };
+	};
 })(jQuery);
