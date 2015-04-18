1abb18dee253571e70042b8beca5637ffeb79e06
Don McCurdy <don.r.mccurdy@gmail.com>
2015-04-17 23:29:08 -0700

    Add NPM+Bower manifests, basic Grunt tasks

---
+++ Gruntfile.js
@@ -0,0 +1,31 @@
+module.exports = function (grunt) {
+
+	grunt.initConfig({
+		pkg: grunt.file.readJSON('package.json'),
+
+		/* JS MINIFICATION
+		**********************************/
+		uglify: {
+			main: {
+				options: { banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n' },
+				files: { 'tokenizer.min.js': 'tokenizer.js' }
+			}
+		},
+
+		/* SEMVER HELPER
+		**********************************/
+		bump: {
+			options: {
+				pushTo: 'origin',
+				files: ['package.json', 'bower.json'],
+				commitFiles: ['package.json', 'bower.json']
+			}
+		}
+
+	});
+
+	grunt.loadNpmTasks('grunt-contrib-uglify');
+	grunt.loadNpmTasks('grunt-bump');
+
+	grunt.registerTask('build', ['uglify']);
+};
