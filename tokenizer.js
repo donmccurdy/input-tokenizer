592d4cef515df4ba387c3952ff4d6fbc326ab8e0
Don McCurdy <don.r.mccurdy@gmail.com>
2016-04-17 16:34:47 -0400

    Proposals 1-7, April 12 2016

--- tokenizer.js
+++ tokenizer.js
@@ -27,9 +27,14 @@
 			separators: [',', ' ', '.'],
 			callback: null,
 			source: null,
+			selectedKeys: null,
 			allowUnknownTags: true,
 			numToSuggest: 5,
-			onclick: null
+			onclick: null,
+			allowDuplicates: false,
+			suggestDuplicates: false,
+			allowFullSearch: true,
+			fullSearchChar: '*'
 		}, argOpts);
 
 		// PRIVATE METHODS
@@ -40,6 +45,7 @@
 			input = argElement;
 			buildHTML();
 			bindEvents();
+			displaySelectedElements();
 		};
 		buildHTML = function () {
 			var ns = options.namespace;
@@ -56,12 +62,26 @@
 			.prepend('<span class="'+ns+'-wrapper-label">'+options.label+'</span>')
 			.find('.'+ns+'-input-wrapper').append(suggestions).end();
 		};
+		displaySelectedElements = function () {
+		    if ($.isArray(options.selectedKeys)) {
+		        for (var i = 0; i < options.selectedKeys.length; i++) {
+		        	var key = options.selectedKeys[i];
+		            if ($.isArray(options.source)) {
+		            	push(key, i);
+		            } else if(typeof options.source == 'object') {		            	
+		            	push(options.source[key], key);
+		            }
+		            
+		        }
+		    }
+		};
 		bindEvents = function () {
 			var ns = options.namespace;
 			wrap
 				.on('focus', 'input', function () {  // On focus, stylize wrapper.
 					wrap.addClass(ns+'-focus');
-				}).on('blur', 'input', function () { // On blur, un-stylize.
+				})
+                .on('blur', 'input', function () { // On blur, un-stylize.
 					wrap.removeClass(ns+'-focus');
 					eventQueue.delay(200).queue().push(function () {
 						// On blur, tag remaining text only if autocomplete is disabled.
@@ -89,6 +109,7 @@
 						if (selected.length) {
 							selected.removeClass(selectClass)
 							.prev('li').add(selected.siblings().last()).eq(0).addClass(selectClass);
+							scrollToView(selected);
 						} else {
 							suggestions.find('li').last().addClass(selectClass);
 						}
@@ -97,11 +118,12 @@
 						if (selected.length) {
 							selected.removeClass(selectClass)
 							.next('li').addClass(selectClass);
+							scrollToView(selected);
 						} else {
 							suggestions.find('li').first().addClass(selectClass);
 						}
 					}
-				}).on('keypress', 'input', function (event) { // Input listener to create tokens.
+				}).on('keypress', 'input', function (event) { // Input listener to create tokens.					
 					if (options.separators.indexOf(String.fromCharCode(event.which)) > -1 || event.which === 13) {
 						event.preventDefault();
 						tryPush(input.val());
@@ -112,6 +134,8 @@
 					if (event.which === 38 || event.which === 40) { return; }
 					if ($.isArray(options.source)) { // Autosuggest from list
 						suggest(options.source);
+					} else if(typeof options.source == 'object') {
+						suggestMap(options.source);
 					} else if (options.source) { // Autosuggest from function
 						options.source(input.val(), suggest);
 					}
@@ -138,21 +162,27 @@
 			tryPush = function (value) {
 				var match = getMatch();
 				if (value && (options.allowUnknownTags || match)) {
-					push(match || value);
+					var ele = match || value;
+					push(ele.text(), ele.attr('data'));
 					input.val('');
 					callback();
 				}
 				suggest([], '');
 			};
-			push = function (value) {
-				var
-				ns = options.namespace,
-				pre = ns+'-token',
-				token = '<div class="'+pre+'" data-token="'+value+'">'+
-				'<span class="'+pre+'-label">'+value.trim()+'</span>'+
-				'<span class="'+pre+'-x">'+options.xContent+'</span>'+
-				'</div>';
-				list.append(token);
+			push = function (value, data) {
+				var firstOccurrence = isFirstOccurrence(value, data);
+	            if(options.allowDuplicates || firstOccurrence) {	            	
+	            	value = value || data;   
+	            	data = data || value;
+					var
+					ns = options.namespace,
+					pre = ns+'-token',
+					token = '<div class="'+pre+'" data-token="'+ data +'">'+
+					'<span class="'+pre+'-label">'+value.trim()+'</span>'+
+					'<span class="'+pre+'-x">'+options.xContent+'</span>'+
+					'</div>';
+					list.append(token);
+	            }
 				return input;
 			};
 			pop = function () {
@@ -197,7 +227,8 @@
 				limit = options.numToSuggest || 1000,
 				list = [];
 				for (i = 0; word && i < words.length && list.length < limit; i++) {
-					if (!words[i].match(re1)) { continue; }
+					if ((!words[i].match(re1) && !(options.allowFullSearch && word == options.fullSearchChar))
+						|| (!options.suggestDuplicates && !isFirstOccurrence(words[i], i))) { continue; }
 					list.push('<li class="'+ns+'-suggest-li'+
 						(words[i].match(re2) ? ' '+ns+'-sel' : '')+'">'+words[i]+'</li>');
 				}
@@ -206,11 +237,63 @@
 				.end()
 				[list.length ? 'addClass' : 'removeClass']('.'+ns+'-vis');
 			};
+			suggestMap = function (words, word) {
+			    word = word === undefined ? input.val() : word;
+			    var
+                i,
+                ns = options.namespace,
+                re1 = new RegExp(escapeRegExp(word), 'i'),
+                re2 = new RegExp('^'+escapeRegExp(word)+'$', 'i'),
+                limit = options.numToSuggest || 1000,
+                list = [];
+
+                var keys = Object.keys(words);
+                for (i = 0; word && i < keys.length && list.length < limit; i++) {
+                    if ((!words[keys[i]].match(re1)
+                        && !(options.allowFullSearch && word == options.fullSearchChar))
+                        || (!options.suggestDuplicates && !isFirstOccurrence(words[keys[i]], keys[i]))) { continue; }
+                    list.push('<li data="'+keys[i]+'" class="'+ns+'-suggest-li'+
+                        (words[keys[i]].match(re2) ? ' '+ns+'-sel' : '')+'">'+words[keys[i]]+'</li>');
+                }
+                suggestions.children('ul')
+                .html(list.join(''))
+                .end()
+                [list.length ? 'addClass' : 'removeClass'](''+ns+'-vis');
+			};
 			getMatch = function () {
-				return suggestions.find('.'+options.namespace+'-sel').eq(0).text();
+				return suggestions.find('.'+options.namespace+'-sel').eq(0);
 			};
 			escapeRegExp = function (str) {
 				return str.replace(/[\-\[\]\/\{\}\(\)\*\+\?\.\\\^\$\|]/g, "\\$&");
+			};
+			isFirstOccurrence = function(str, data) {
+			    var tokens = get();
+			    for (var i = 0; i < tokens.length; i++) {
+			        if (tokens[i] == str || tokens[i] == data) {
+			            return false;
+			        }
+			    }
+				return true;
+			};
+			scrollToView = function(selected) {
+                parent = selected.parent();
+
+                var selectClass = options.namespace+'-sel',
+                suggestionHeight = suggestions.find('li').first().outerHeight(),
+                selectedIdx = $('.'+options.namespace+'-suggest ul li.'+selectClass).index(),
+                selectedOffset = selectedIdx * suggestionHeight,
+                offset = selectedOffset + parent.scrollTop(),
+                offset_end = offset + selected.outerHeight();
+
+                var visible_area_start = parent.scrollTop();
+                var visible_area_end = visible_area_start + parent.outerHeight();
+
+                if (selectedOffset < visible_area_start) {
+                    selected.parent()[0].scrollTop = selectedOffset;
+                } else if ((selectedOffset + suggestionHeight) > visible_area_end) {
+                    selected.parent()[0].scrollTop = offset_end - visible_area_end;
+                }
+                return true;
 			};
 
 			init (argElement);
